f98f049c23e8098d4f39ed5311da45f68317fc92
konate Minsitan <konateminsitan@gmail.com>
2019-11-15 15:19:50 +0100

    First commit

---
+++ src/main/java/com/example/Tp1Spring/ServerClientGestionStockProduitApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tp1Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerClientGestionStockProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerClientGestionStockProduitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tp1Spring/ServerClientGestionStockProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tp1Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerClientGestionStockProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
